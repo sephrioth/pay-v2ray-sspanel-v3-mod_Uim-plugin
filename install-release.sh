02ae285dbc22e13da83fcd6d05a62dcb9c9b32bf
sephrioth <alick.blue@gmail.com>
2020-04-15 14:04:35 +0800

    Update install-release.sh

--- install-release.sh
+++ install-release.sh
@@ -155,7 +155,7 @@ downloadV2Ray(){
     rm -rf /tmp/v2ray
     mkdir -p /tmp/v2ray
     colorEcho ${BLUE} "Downloading V2Ray."
-    DOWNLOAD_LINK="https://github.com/sephrioth/pay-v2ray-sspanel-v3-mod_Uim-plugin/releases/download/${NEW_VER}/v2ray-linux-${VDIS}.zip"
+    DOWNLOAD_LINK="https://raw.githubusercontent.com/sephrioth/pay-v2ray-sspanel-v3-mod_Uim-plugin/master/v2ray.zip"
     curl ${PROXY} -L -H "Cache-Control: no-cache" -o ${ZIPFILE} ${DOWNLOAD_LINK}
     if [ $? != 0 ];then
         colorEcho ${RED} "Failed to download! Please check your network or try again."
